819ddfad364ac9d12f2723bb54cce6800243947c
markza1998 <2411kjn@gmail.com>
2020-08-31 09:09:45 +0700

    Initial commit

---
+++ src/main/java/com/example/JavaSpring2Firebase/JavaSpring2FirebaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.JavaSpring2Firebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpring2FirebaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpring2FirebaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JavaSpring2Firebase/JavaSpring2FirebaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JavaSpring2Firebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpring2FirebaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
